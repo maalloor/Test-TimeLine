20c8a38adc0b0b57f390a4cec256a2a6f370976c
maalloor <maalloor@espol.edu.ec>
2021-12-28 21:00:14 -0500

    Importaciones en app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TimelineElement } from './time-line/timeline-element';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'time-line';
+  content = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum praesentium officia, fugit recusandae 
+  ipsa, quia velit nulla adipisci? Consequuntur aspernatur at, eaque hic repellendus sit dicta consequatur quae, 
+  ut harum ipsam molestias maxime non nisi reiciendis eligendi! Doloremque quia pariatur harum ea amet quibusdam 
+  quisquam, quae, temporibus dolores porro doloribus.`;
+
+  timeline: TimelineElement[] = [];
+
+  load() {
+    this.timeline = [];
+    setTimeout(() => { // simulate delay
+      this.timeline = [
+    { caption: '16 Jan', date: new Date(2014, 1, 16), title: 'Horizontal Timeline', content: this.content },
+    { caption: '28 Feb', date: new Date(2014, 2, 28), title: 'Status#1', content: this.content },
+    { caption: '20 Mar', date: new Date(2014, 3, 20), selected: true, title: 'Status#2', content: this.content },
+    { caption: '20 May', date: new Date(2014, 5, 20), title: 'Status#3', content: this.content },
+    { caption: '09 Jul', date: new Date(2014, 7, 9), title: 'Status#4', content: this.content },
+    { caption: '30 Aug', date: new Date(2014, 8, 30), title: 'Status#5', content: this.content },
+    { caption: '15 Sep', date: new Date(2014, 9, 15), title: 'Status#6', content: this.content },
+    { caption: '01 Nov', date: new Date(2014, 11, 1), title: 'Status#7', content: this.content },
+    { caption: '10 Dec', date: new Date(2014, 12, 10), title: 'Status#8', content: this.content },
+    { caption: '29 Jan', date: new Date(2015, 1, 19), title: 'Status#9', content: this.content },
+    { caption: '3 Mar', date: new Date(2015, 3, 3), title: 'Status#9', content: this.content },
+    ];
+    }, 2000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TimeLineComponent } from './time-line/time-line.component';
 import { DetalleRequerimientoComponent } from './detalle-requerimiento/detalle-requerimiento.component';
@@ -10,11 +11,13 @@ import { DetalleRequerimientoComponent } from './detalle-requerimiento/detalle-r
   declarations: [
     AppComponent,
     TimeLineComponent,
+    FormsModule,
     DetalleRequerimientoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
