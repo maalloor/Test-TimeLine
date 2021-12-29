ab613693897a4d7de7aa0b801ef6cd5d488377c6
maalloor <maalloor@espol.edu.ec>
2021-12-28 19:42:18 -0500

    Creacion de componente time-line

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimeLineComponent } from './time-line/time-line.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimeLineComponent,
   ],
   imports: [
     BrowserModule,
